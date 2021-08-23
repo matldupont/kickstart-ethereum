4b0ab59a79c203eb6b454586d6c7674f3c5a1c63
Mat Dupont <dupont.mat.l@gmail.com>
2021-08-23 15:17:41 -0400

    Campaign tests

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -36,4 +36,65 @@ describe("Campaigns", () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
+
+  it("marks caller as the campaign manager", async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.strictEqual(accounts[0], manager);
+  });
+
+  it("allows people to contribute money and marks them as approvers", async () => {
+    await campaign.methods
+      .contribute()
+      .send({ from: accounts[1], value: "200" });
+
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributor);
+  });
+
+  it("requires a minimum contribution", async () => {
+    try {
+      await campaign.methods
+        .contribute()
+        .send({ from: accounts[1], value: "5" });
+    } catch (err) {
+      assert(err);
+      return;
+    }
+
+    assert(false);
+  });
+
+  it("allows a manager to make a payment request", async () => {
+    await campaign.methods
+      .createRequest("pay the man", "10000", accounts[2])
+      .send({ from: accounts[0], gas: "1000000" });
+
+    const request = await campaign.methods.requests(0).call();
+    assert.strictEqual("pay the man", request.description);
+  });
+
+  it("processes requests", async () => {
+    await campaign.methods
+      .contribute()
+      .send({ from: accounts[1], value: web3.utils.toWei("10", "ether") });
+
+    await campaign.methods
+      .createRequest("pay the man", web3.utils.toWei("5", "ether"), accounts[2])
+      .send({ from: accounts[0], gas: "1000000" });
+
+    await campaign.methods
+      .approveRequest(0)
+      .send({ from: accounts[1], gas: "1000000" });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: "1000000",
+    });
+
+    let balance = await web3.eth.getBalance(accounts[2]);
+    balance = web3.utils.fromWei(balance, "ether");
+    balance = parseFloat(balance);
+
+    assert(balance > 104);
+  });
 });
