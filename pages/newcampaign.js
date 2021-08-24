4e0be41fe21abfce82b9952aba5950ef894cb22d
Mat Dupont <dupont.mat.l@gmail.com>
2021-08-24 09:56:01 -0400

    added nextjs scaffolding

---
+++ pages/newcampaign.js
@@ -0,0 +1,5 @@
+function NewCampaignPage() {
+  return <h1>This is the new campaign page</h1>;
+}
+
+export default NewCampaignPage;
---
+++ pages/show.js
@@ -0,0 +1,5 @@
+function ShowPage() {
+  return <h1>This is the show page</h1>;
+}
+
+export default ShowPage;
